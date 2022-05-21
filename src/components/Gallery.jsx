8ca2ae56646db76b849ed2c9aa27f4d8eabe0bee
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-05-21 18:58:09 -0300

    + Add more images in about component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -17,20 +17,29 @@ export default function Gallery() {
   return (
     <Slider {...settings} className="slider">
       <div>
+        <img src="https://i.ibb.co/3MrPZ6s/oie-21235143b9e2-O2-HX.jpg" alt="img-1" />
+      </div>
+      <div>
+        <img src="https://i.ibb.co/6JNDvbk/oie-n7t-JHWLAt-WQ8.png" alt="img-2" />
+      </div>
+      <div>
         <img
           src="https://i.ibb.co/pJ4t651/oie-c-PDZk-R2-IU0y3.jpg"
-          alt="img-1"
+          alt="img-3"
         />
       </div>
       <div>
         <img
           src="https://i.ibb.co/72g3g9c/oie-QRUr-Oxvnl-Gu-O.jpg"
           alt="img-2"
-          className="img-2"
+          className="img-4"
         />
       </div>
       <div>
-        <img src="https://i.ibb.co/6JNDvbk/oie-n7t-JHWLAt-WQ8.png" alt="img-3" />
+        <img src="https://i.ibb.co/6DhcBTS/20211220-234332.png" alt="img-5" />
+      </div>
+      <div>
+        <img src="https://i.ibb.co/44dmVbh/oie-21235044h-El-Bw8sj.jpg" alt="img-6" />
       </div>
     </Slider>
   );
