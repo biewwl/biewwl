3bff32c09a05a1e5aff2b81608b4b69359a94e78
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-06-04 16:31:19 -0300

    * Change App class > function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
@@ -6,18 +6,16 @@ import Projects from "./routes/Projects";
 import "./colors/colors.css";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route exact path="/about" component={About}></Route>
-          <Route exact path="/projects" component={Projects}></Route>
-        </Switch>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/projects" component={Projects}></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
