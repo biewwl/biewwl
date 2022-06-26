e8c2ad876d4821b5c0f31c69842b5d124b5b624b
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-06-25 23:31:03 -0300

    * New home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,35 +1,87 @@
 import React from "react";
-import Header from "../../components/Header";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import invert from "../../helpers/invertTheme";
 import "./styles/Home.css";
 import "./styles/Home-mobile.css";
+import { changeColor, changeTheme } from "../../redux/actions/themeAction";
+import biewwl from "./images/biewwl.jpg";
 
-function Home({ color, theme }) {
+function Home({ color, theme, dispatch }) {
+  const changeInvertTheme = () => {
+    if (theme === "" && color === "-dark") {
+      dispatch(changeTheme("-dark"));
+      dispatch(changeColor(""));
+    } else if (theme === "-dark" && color === "") {
+      dispatch(changeTheme(""));
+      dispatch(changeColor("-dark"));
+    } else if (theme === "") {
+      dispatch(changeTheme("-dark"));
+    } else {
+      dispatch(changeTheme(""));
+    }
+  };
   return (
     <>
-      <Header selectedPage="home" />
+      {/* <Header selectedPage="home" /> */}
       <main className={`bg${theme} home`}>
-        <section className="home-introduction-container">
-          <section className={`home-introduction-content c${invert(theme)}`}>
-            <span className="i-am">I am</span>
-            <h1 className={`name c${color}`}>Gabriel Dias</h1>
-            <span className="web-developer">Web Developer</span>
+        <section className={`card-left${theme}`}>
+          <h1>biewwl</h1>
+          <section className={`home-introduction`}>
+            <span>Welcome!</span>
+            <h2>
+              Follow my projects developed in my trajectory as a WEB developer
+            </h2>
             <Link
               to="/projects"
-              className={`projects-button bg${color} c${theme}`}
+              className={`home-button bg${theme} c${invert(theme)}`}
             >
-              Projects
+              Explore projects
             </Link>
           </section>
         </section>
-        <section className="home-image-container">
-          <div
-            className={`bg${color} home-image`}
-            data-testid="homeImage"
-          ></div>
+        <section className={`card-right${theme}`}>
+          <div className="top-content">
+            <section className="header-card-right">
+              <span className="star-home">✦</span>
+              <img src={biewwl} alt="" />
+            </section>
+            <section className="message-container">
+              <h3 className={`message c${invert(theme)}`}>
+                Feel free to explore the projects!
+              </h3>
+              <input type="text" placeholder="Name, library..." />
+            </section>
+          </div>
+          <div className="bottom-content">
+            <nav>
+              <Link to="/about" className={`c${invert(theme)}`}>
+                About
+              </Link>
+              <Link to="/projects" className={`c${invert(theme)}`}>
+                Projects
+              </Link>
+            </nav>
+            <p className={`about-home c${invert(theme)}`}>
+              Since I was very young I have always been fascinated by virtual
+              worlds and at the beginning of 2022 I joined a web development
+              school. I learned to love programming and became very good at
+              it...
+            </p>
+          </div>
         </section>
+        {theme === "" && (
+          <span
+            className={`ri-moon-fill`}
+            onClick={changeInvertTheme}
+          />
+        )}
+        {theme === "-dark" && (
+          <span
+            className={`ri-sun-fill`}
+            onClick={changeInvertTheme}
+          />
+        )}
       </main>
     </>
   );
