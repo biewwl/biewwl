4e06e1dd37dfcdeb26cf0093ebf47d18c45c1417
Gabriel Dias <gabriel.dias.fern@gmail.com>
2022-10-15 13:04:56 -0300

    FEAT: Animation on home

--- src/components/TopicAbout/index.jsx
+++ src/components/TopicAbout/index.jsx
@@ -2,16 +2,11 @@ import { connect } from "react-redux";
 import { invert } from "../../helpers/theme";
 import "./styles/TopicAbout.css";
 
-function TopicAbout({ color, theme }) {
+function TopicAbout({ color, theme, title, content }) {
   return (
     <article className="topic-about">
-      <h3 className={`c${color}`}>About React</h3>
-      <p className={`c${invert(theme)}`}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus ipsum
-        accusantium odio ratione, quis blanditiis maiores id error est,
-        distinctio, itaque non! Temporibus, nisi suscipit? Praesentium autem
-        esse sed repellendus?
-      </p>
+      <h3 className={`c${color}`}>{title}</h3>
+      <p className={`c${invert(theme)}`}>{content}</p>
     </article>
   );
 }
